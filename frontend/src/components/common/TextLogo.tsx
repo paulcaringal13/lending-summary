18790dfe8e0ba847a44fa518326da992e9ba45a4
Paul Daniel Caringal <daniel.caringal@opsolutions.biz>
2024-10-13 22:26:41 +0800

    LS-7 | Logo Adjustments

--- frontend/src/components/common/TextLogo.tsx
+++ frontend/src/components/common/TextLogo.tsx
@@ -2,12 +2,11 @@ import { Label } from "../ui/label";
 
 const TextLogo = () => {
   return (
-    <Label className="text-xl text-center font-extrabold my-3 sm:my-4 md:text-2xl lg:text-3xl xl:my-6 tracking-[-0.5px] leading-none">
-      <span className="mr-14">Lending</span>
-      <span className="block leading-none text-primary tracking-[-0.5px] ml-10">
-        Summary
-      </span>
-    </Label>
+    <div className="flex flex-col text-primary text-center w-fit mx-auto leading-6 tracking-widest">
+      <Label className="font-extrabold text text-3xl font-baskerville">
+        treblend
+      </Label>
+    </div>
   );
 };
 
