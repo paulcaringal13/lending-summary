a39c16b3ad8f3223b2ed07d5d41ff513637d3ad5
Paul Daniel Caringal <daniel.caringal@opsolutions.biz>
2024-11-01 19:36:04 +0800

    LS-7 | Fix on Port and Checkbox

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	server: {
+		port: 3001,
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
